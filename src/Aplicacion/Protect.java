92f579bff0264c924e04671034eda157289b308b
Smith <Smitherdezz@hotmail.com>
2022-11-15 18:44:20 -0600

    impresion en JOptionPane

--- src/Aplicacion/Protect.java
+++ src/Aplicacion/Protect.java
@@ -1,4 +1,5 @@
 package Aplicacion;
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -7,8 +8,7 @@ import java.io.IOException;
 //El proyeto tiene como entrada un archivo llamado readme y da como salida la cantidad
 //de parrafos que contiene
 public class Protect {
-    public static void main (String[] args) throws IOException, ClassNotFoundException
-    {
+    public static void main (String[] args) throws IOException, ClassNotFoundException {
         String contador;
         File archivo = new File ("C:\\Users\\jaher\\IdeaProjects\\Automatas I\\src\\Aplicacion\\readme.txt");
         FileReader flujoEntrada = new FileReader(archivo);
@@ -18,6 +18,6 @@ public class Protect {
             i++;
         }
         System.out.println("Numero de lineas " + i);
-
+        JOptionPane.showInputDialog("El numero de lineas son " + i );
     }
 }
